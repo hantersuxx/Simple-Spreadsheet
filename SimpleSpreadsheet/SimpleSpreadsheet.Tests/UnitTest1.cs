a1ccde12c052576ac04cdb7f3ee2676660956308
Aliaksandr Joya <Aliaksandr_Joya@epam.com>
2020-05-12 12:55:09 +0300

    Add initial project; Add .gitignore.

---
+++ SimpleSpreadsheet/SimpleSpreadsheet.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleSpreadsheet.Tests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
